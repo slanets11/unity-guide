ee1c810fa403814bd7e46ca576778678f121ff45
Axonny <KawaiiAxonny@gmail.com>
2021-03-20 11:42:06 +0500

    Step 7: Gem spawn

--- Assets/Scripts/Gem.cs
+++ Assets/Scripts/Gem.cs
@@ -1,14 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Gem : MonoBehaviour
 {
+    internal GemSpawner gemSpawner;
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Debug.Log("Gem Collected!");
+            gemSpawner.SpawnGem();
             Destroy(gameObject);
         }     
     }
---
+++ Assets/Scripts/GemSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class GemSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject gemPrefab;
+    [SerializeField] private Transform[] spawnPositions;
+
+    private int lastPosition = -1;
+
+    private void Start()
+    {
+        SpawnGem();
+    }
+
+    public void SpawnGem()
+    {
+        while (true)
+        {
+            var next = Random.Range(0, spawnPositions.Length);
+            if (next == lastPosition)
+                continue;
+            lastPosition = next;
+            var gem = Instantiate(gemPrefab, spawnPositions[next].position, Quaternion.identity).GetComponent<Gem>();
+            gem.gemSpawner = this;
+            return;
+        }
+    }
+}
